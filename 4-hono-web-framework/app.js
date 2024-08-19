605845a48eb2873eab25c74eee38a3bb643ffb33
Anna Sokolova <a.sokolova.dev@gmail.com>
2024-08-19 16:21:58 +0300

    first commit

---
+++ 4-hono-web-framework/app.js
@@ -0,0 +1,28 @@
+import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
+
+const app = new Hono();
+
+app.get("/restaurants", c => c.text("Listing restaurants."))
+app.post("/restaurants", c => c.text("Adding a restaurant."))
+app.get("/restaurants/:id", c => {
+    const id = c.req.param("id");
+    return c.text(`Showing restaurant with id ${id}.`)
+})
+
+app.get("/restaurants/:id/reviews", c => {
+    const id = c.req.param("id");
+    return c.text(`Listing reviews for restaurant with id ${id}.`)
+})
+
+app.post("/restaurants/:id/reviews", c => {
+    const id = c.req.param("id");
+    return c.text(`Adding a review for restaurant with id ${id}.`)
+})
+
+app.on("DELETE", "/restaurants/:id/reviews/:rid", c => {
+    const id = c.req.param("id");
+    const rid = c.req.param("rid");
+    return c.text(`Removing review ${rid} from restaurant with id ${id}.`);
+})
+
+export default app
