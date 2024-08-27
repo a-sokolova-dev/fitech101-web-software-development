414725f999e4449843032b7a232bd3e189e7abad
Anna Sokolova <a.sokolova.dev@gmail.com>
2024-08-27 11:09:42 +0300

    add part 11

---
+++ 11-data-validation/11.4-error-partials/assignment-1/bookController.js
@@ -0,0 +1,67 @@
+import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
+import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
+
+import * as bookService from "./bookService.js";
+const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
+
+const bookValidator = z.object({
+  name: z.string().min(3, {
+    message: "The book name should be a string of at least 3 characters.",
+  }),
+  pages: z.coerce
+    .number()
+    .min(1, {
+      message: "The number of pages should be a number between 1 and 1000.",
+    })
+    .max(1000, {
+      message: "The number of pages should be a number between 1 and 1000.",
+    }),
+  isbn: z
+    .string()
+    .length(13, { message: "The ISBN should be a string of 13 characters." }),
+});
+
+const showForm = async (c) => {
+  return c.html(
+    eta.render("books.eta", { books: await bookService.listBooks() })
+  );
+};
+
+const showBook = async (c) => {
+  const id = c.req.param("id");
+  return c.html(
+    eta.render("book.eta", { book: await bookService.getBook(id) })
+  );
+};
+
+const createBook = async (c) => {
+  const body = await c.req.parseBody();
+  const validationResult = bookValidator.safeParse(body);
+  if (!validationResult.success) {
+    return c.html(
+      eta.render("books.eta", {
+        ...body,
+        errors: validationResult.error.format(),
+        books: await bookService.listBooks(),
+      })
+    );
+  }
+
+  await bookService.createBook(body);
+  return c.redirect("/books");
+};
+
+const updateBook = async (c) => {
+  const id = c.req.param("id");
+  const body = await c.req.parseBody();
+  await bookService.updateBook(id, body);
+  return c.redirect(`/books/${id}`);
+};
+
+const deleteBook = async (c) => {
+  const id = c.req.param("id");
+  await bookService.deleteBook(id);
+  return c.redirect("/books");
+};
+
+export { createBook, deleteBook, showForm, showBook, updateBook };
---
+++ 11-data-validation/11.4-error-partials/assignment-2/app.js
@@ -0,0 +1,27 @@
+import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
+
+import * as feedbacks from "./feedbacks.js";
+import * as courseController from "./courseController.js";
+
+const app = new Hono();
+
+app.get("/courses/:courseId/feedbacks/:feedbackId", async (c) => {
+  const courseId = c.req.param("courseId");
+  const feedbackId = c.req.param("feedbackId");
+  const feedbackCount = await feedbacks.getFeedbackCount(courseId, feedbackId);
+  return c.text(`Feedback ${feedbackId}: ${feedbackCount}`);
+});
+
+app.post("/courses/:courseId/feedbacks/:feedbackId", async (c) => {
+  const courseId = c.req.param("courseId");
+  const feedbackId = c.req.param("feedbackId");
+  await feedbacks.incrementFeedbackCount(courseId, feedbackId);
+  return c.redirect(`/courses/${courseId}`);
+});
+
+app.get("/courses", courseController.showForm);
+app.get("/courses/:id", courseController.showCourse);
+app.post("/courses", courseController.createCourse);
+app.post("/courses/:id/delete", courseController.deleteCourse);
+
+export default app;
